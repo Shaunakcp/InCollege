e469e2f1ab1cb0477f62bc98f82fbef5828330be
aaronPhm412 <92625419+aaronPhm412@users.noreply.github.com>
2022-11-11 19:25:41 -0500

    everything integrated

--- incollege.py
+++ incollege.py
@@ -1,10 +1,6 @@
 import sqlite3           # database used to store account & job information
 import datetime
 
-
-class Notifications:    # create new class for notifications
-    pass
-
 class Message:
     def __init__(self, dbName):
         self._db = sqlite3.connect(f"./{dbName}.db")
